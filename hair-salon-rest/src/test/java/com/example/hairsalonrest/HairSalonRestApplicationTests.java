e6fea9ceecd94f4259f3a2341d41bc7a5cc4634e
idamanukyan <ida_manukyan@edu.aua.am>
2021-10-30 14:56:29 +0400

    test packages

---
+++ hair-salon-rest/src/test/java/com/example/hairsalonrest/HairSalonRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hairsalonrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairSalonRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
